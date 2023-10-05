8525d38d2b661ab4e5c2b80c8e79b6c3555e1a02
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-10-05 23:21:16 +0100

    python annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+A function that takes a float a and a float b as argument and return their sum as float
+"""
+
+def add(a: float, b: float) -> float :
+  """
+  Add two float numbers a and b
+
+  Args:
+    a (float): The first float number
+    b (float): The second float number
+
+    Returns:
+      floats: The sum of a and b
+  """
+  return a + b
---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+"""
+A script that defines a function to create a multiplier function
+"""
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Create a multiplier function
+    Args:
+        multiplier (float): The multiplier float number
+
+    Returns:
+        Callable[[float], float]: A function that multiplies a float by the specified multiplier.
+    """
+
+    def multiplier_function(x: float) -> float:
+        """
+        Multiply a float by the specified multiplier
+
+        Args:
+            x (float): the float number argument
+
+        Returns:
+            float: The result of multiplying x by the multiplier
+        """
+        return x * multiplier
+    return multiplier_function
