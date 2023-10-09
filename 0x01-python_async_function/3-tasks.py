838bc39e0c3582ff5134b3902a37543b46282abf
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-10-09 22:00:18 +0100

    python async function

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -4,10 +4,11 @@ A script that returns asyncio.Task
 """
 
 import asyncio
+from asyncio import Task
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int):
+def task_wait_random(max_delay: int) -> Task:
     """
     Describe a function that returns asyncio.Task
     """
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-
+A script that executes multiple coroutines
 """
 
 import asyncio
