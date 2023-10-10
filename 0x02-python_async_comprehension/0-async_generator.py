88f8b0e8cec5c0fb77590c1e832d924db261cdc9
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-10-10 18:25:03 +0100

    python async comprehension

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+A script that describe a coroutine with no arguments
+"""
+
+import asyncio
+import random
+
+async def async_generator() -> float:
+    """
+    A coroutine that loops 10 times, wait for 
+    one second and output a random number
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
