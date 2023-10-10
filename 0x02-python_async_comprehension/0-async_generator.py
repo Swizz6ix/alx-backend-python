1c700d6e2b7ca1cf0c06d178d6c7d6d6f1393645
PlayBoi Baby <ferdinandcharles6@gmail.com>
2023-10-10 18:27:24 +0100

    python async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,6 +6,7 @@ A script that describe a coroutine with no arguments
 import asyncio
 import random
 
+
 async def async_generator() -> float:
     """
     A coroutine that loops 10 times, wait for 
