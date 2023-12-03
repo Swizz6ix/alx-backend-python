e268e7d3dfe914a390bd6177f3be29e7d8dbe2db
swizz6ix <ferdinandcharles6@gmail.com>
2023-12-03 21:03:47 +0100

    unittests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,12 +3,10 @@
 This module contains unittest test for utils module
 """
 import unittest
-from typing import Dict, Tuple, Union
+from typing import Dict, Tuple, Union, Mapping, Sequence, Any, Callable
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import (
-    access_nested_map, get_json, memoize,
-)
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -22,10 +20,10 @@ class TestAccessNestedMap(unittest.TestCase):
         ({'a': {'b': 2}}, ('a', 'b'), 2),
     ])
     def test_access_nested_map(self,
-                                nested_map: Dict,
-                                path: Tuple[str],
+                                nested_map: Mapping,
+                                path: Sequence,
                                 expected: Union[Dict, int]
-                                ) -> None:
+                            ) -> None:
         """
         Test the method access_nested_map
         """
